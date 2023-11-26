3f212a04bbe0df66fdaabaa5811c3acbd900998d
Patric Stout <github@truebrain.nl>
2023-11-26 13:09:05 +0100

    feat: add Discord and donation link in the banner

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -8,14 +8,20 @@ export const Banner = () => {
     <div className={styles.banner}>
       EVEShip.fit - View, Create, and Share your EVE Online ship fits online
       <div className={styles.bannerLinks}>
-        <a href="https://github.com/EVEShipFit">
-          <Image src="/github-mark.svg" alt="GitHub" width={32} height={32} />
+        <a href="https://github.com/EVEShipFit/#donations" target="_new">
+          <Image src="/plex.png" alt="Donation" width={32} height={32} title="Donate to keep eveship.fit running" />
+        </a>
+        <a href="https://discord.gg/S5V5BkvNf7" target="_new">
+          <Image src="/discord.svg" alt="Discord" width={32} height={32} title="Join us on Discord!" />
+        </a>
+        <a href="https://github.com/EVEShipFit" target="_new">
+          <Image src="/github-mark-white.svg" alt="GitHub" width={32} height={32} title="View source-code on GitHub" />
         </a>
       </div>
     </div>
     <div className={styles.bannerConstruction}>
       This website is currently a work in progress; not all functionalities are available yet.<br/>
-      Found a bug? Please <a href="https://github.com/EVEShipFit/roadmap/issues" >report it here</a>.
+      Found a bug? Please <a href="https://github.com/EVEShipFit/roadmap/issues" target="_new">report it here</a>.
     </div>
   </>
 }
