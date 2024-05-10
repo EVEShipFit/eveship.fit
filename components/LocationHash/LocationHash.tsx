17337f65ecb049016d51898323a23f7bd78f62ed
Patric Stout <github@truebrain.nl>
2024-05-10 23:23:03 +0200

    feat: keep the URL in the browser updated with the current fit (#46)
    This should make sharing fit even easier.

--- components/LocationHash/LocationHash.tsx
+++ components/LocationHash/LocationHash.tsx
@@ -1,9 +1,8 @@
-import { type EsiFit, eveShipFitHash, ShipSnapshotContext } from "@eveshipfit/react";
+import { type EsiFit, eveShipFitHash, ShipSnapshotContext, useEveShipFitLinkHash } from "@eveshipfit/react";
 import React from "react";
 
 async function analyzeHash(changeFit: (fit: EsiFit) => void) {
   const hash = window.location.hash;
-  window.history.replaceState(null, "", window.location.pathname + window.location.search);
 
   if (hash.startsWith("#fit:")) {
     const fitHash = hash.slice(1);
@@ -16,6 +15,7 @@ async function analyzeHash(changeFit: (fit: EsiFit) => void) {
 
 export const LocationHash = () => {
   const snapshot = React.useContext(ShipSnapshotContext);
+  const eveShipFitLinkHash = useEveShipFitLinkHash();
 
   React.useEffect(() => {
     analyzeHash(snapshot.changeFit);
@@ -23,6 +23,12 @@ export const LocationHash = () => {
     // We only want to analyze the hash on page-enter; never again after.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  React.useEffect(() => {
+    if (eveShipFitLinkHash === "") return;
+
+    window.history.replaceState(null, "", window.location.pathname + window.location.search + eveShipFitLinkHash);
+  }, [eveShipFitLinkHash]);
 
   if (typeof window !== "undefined") {
     window.addEventListener("hashchange", async () => {
