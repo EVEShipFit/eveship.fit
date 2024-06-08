8ec66932a0bb11974a5c34f6f7df865b18fb401f
Patric Stout <github@truebrain.nl>
2024-06-08 13:40:26 +0200

    feat: change URL schema from "#fit:" to "?fit="

--- components/LocationHash/LocationHash.tsx
+++ components/LocationHash/LocationHash.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 
-import { useEveData, useExportEveShipFitHash, useFitManager, useImportEveShipFitHash } from "@eveshipfit/react";
+import { useEveData, useExportEveShipFit, useFitManager, useImportEveShipFit } from "@eveshipfit/react";
 
 export const LocationHash = () => {
   const eveData = useEveData();
   const fitManager = useFitManager();
-  const importEveShipFitHash = useImportEveShipFitHash();
-  const eveShipFitHash = useExportEveShipFitHash(true);
+  const importEveShipFit = useImportEveShipFit();
+  const encodedESF = useExportEveShipFit(false);
 
   const [requestedFit, setRequestedFit] = React.useState<string | undefined>(undefined);
   const [firstLoad, setFirstLoad] = React.useState(true);
   const setFit = fitManager.setFit;
 
   const analyzeHash = React.useCallback(async () => {
-    const hash = window.location.hash;
-
-    if (hash.startsWith("#fit:")) {
-      const fitHash = hash.slice(1);
+    const oldHash = window.location.hash;
+    if (oldHash.startsWith("#fit:")) {
+      const fitHash = oldHash.slice(5);
       setRequestedFit(fitHash);
+      return;
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const hash = urlParams.get("fit");
+    if (hash !== null && hash.length > 1) {
+      setRequestedFit(hash);
     }
   }, []);
 
@@ -26,7 +32,7 @@ export const LocationHash = () => {
       if (eveData === null || requestedFit === undefined) return;
       setRequestedFit(undefined);
 
-      const fit = await importEveShipFitHash(requestedFit);
+      const fit = await importEveShipFit(requestedFit);
 
       if (fit !== undefined && fit !== null) {
         setFit(fit);
@@ -34,7 +40,7 @@ export const LocationHash = () => {
     }
 
     run();
-  }, [eveData, requestedFit, setFit, importEveShipFitHash]);
+  }, [eveData, requestedFit, setFit, importEveShipFit]);
 
   if (firstLoad) {
     setFirstLoad(false);
@@ -42,10 +48,10 @@ export const LocationHash = () => {
   }
 
   React.useEffect(() => {
-    if (eveShipFitHash === null) return;
+    if (encodedESF === null) return;
 
-    window.history.replaceState(null, "", window.location.pathname + window.location.search + eveShipFitHash);
-  }, [eveShipFitHash]);
+    window.history.replaceState(null, "", `${window.location.pathname}?fit=${encodedESF}`);
+  }, [encodedESF]);
 
   if (typeof window !== "undefined") {
     window.addEventListener("hashchange", async () => {
